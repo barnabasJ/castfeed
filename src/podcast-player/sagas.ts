a0b4998e0602f456e63ff1527d7b9894a608f2e6
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-05-17 13:02:50 +0200

    play new file if player has no status

--- src/podcast-player/sagas.ts
+++ src/podcast-player/sagas.ts
@@ -1,14 +1,20 @@
 import { throttle, select, put } from 'redux-saga/effects'
-import { selectStatus, play, pause } from 'src/player'
+import { selectStatus, play, pause, playNewFile } from 'src/player'
 import { PlaybackStatus } from 'expo-av/build/AV'
 import { togglePlay } from '.'
+import { selectCurrentEpisode, Episode } from 'src/episodes'
 
 function * handelTogglePlay () {
+  const currentEpisode: Episode | null = yield select(selectCurrentEpisode)
   const status: PlaybackStatus | null = yield select(selectStatus)
-  if (status && status.isLoaded && status.isPlaying) {
-    yield put(pause())
-  } else {
-    yield put(play())
+  if (currentEpisode) {
+    if (!status) {
+      yield put(playNewFile(currentEpisode.file))
+    } else if (status && status.isLoaded && status.isPlaying) {
+      yield put(pause())
+    } else {
+      yield put(play())
+    }
   }
 }
 
